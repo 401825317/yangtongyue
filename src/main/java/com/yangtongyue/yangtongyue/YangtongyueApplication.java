f49d20c5d9a79503a172b1f1988c45826a1d46bf
yangjunfei <401825317@qq.com>
2020-02-21 00:21:32 +0800

    init

---
+++ src/main/java/com/yangtongyue/yangtongyue/YangtongyueApplication.java
@@ -0,0 +1,13 @@
+package com.yangtongyue.yangtongyue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YangtongyueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YangtongyueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yangtongyue/yangtongyue/YangtongyueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yangtongyue.yangtongyue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YangtongyueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
